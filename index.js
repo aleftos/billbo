9d692b7fe39f8c7674e980f77fe233265ca936d4
Aleftos <19560228+aleftos@users.noreply.github.com>
2019-03-27 11:02:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,41 +4,9 @@
  * Date: March, 2019
  * GitHub: https://github.com/aleftos/billbo
  * Summary: Utilities for the national tax billing system of Bolivian country.
- * Description:
- *   NIT_EMISOR,
- *   NÚMERO_FACTURA,
- *   FECHA_HORA = getCUFTime(),    // default = getTime
- *   SUCURSAL = 0,                 // default = CASA MATRÍZ
- *   MODALIDAD = 1,                // default = ELECTRÓNICA
- *   TIPO_EMISION = 1,             // default = ONLINE
- *   CÓDIGO_DOCUMENTO_FISCAL = 1,  // default = FACTURA
- *   TIPO_DOCUMENTO_SECTOR = 1,    // default = FACTURA ESTÁNDAR
- *   POS = 0                       // default = NO CORRESPONDE
 */
-let cuf = require('./lib/cuf');
+const cuf = require('./lib/cuf');
 
-console.log(
-  cuf.cuf(
-    '123456789',
-    '1',
-    '20190113163721231'
-  )
-)
-
-console.log(
-  cuf.getCUF({
-    NIT_EMISOR: '123456789',
-    NÚMERO_FACTURA: '1',
-    FECHA_HORA: '20190113163721231'
-  })
-)
-
-console.log(
-  cuf.getCUF({
-    NIT_EMISOR: '123456789',
-    NÚMERO_FACTURA: '1',
-    FECHA_HORA: cuf.getCUFTime()
-  })
-)
-
-console.log(cuf.getCUFTime());
+module.exports = {
+  cuf
+}
